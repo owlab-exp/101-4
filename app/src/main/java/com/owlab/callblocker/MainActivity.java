a3430c7ec452d427b197e73d78dc63c85ba3e2d1
Nemo HJ Lee <hunjae.lee@gmail.com>
2016-06-24 11:39:44 +0900

    Fixed: Crash caused by fragment transaction in onRequestPermissionsResult, after onSaveInstanceState

--- app/src/main/java/com/owlab/callblocker/MainActivity.java
+++ app/src/main/java/com/owlab/callblocker/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
@@ -17,6 +19,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.owlab.callblocker.fragment.AddByManualDialogFragment;
 import com.owlab.callblocker.fragment.AddFromCallLogFragment;
@@ -24,6 +27,7 @@ import com.owlab.callblocker.fragment.AddFromContactsFragment;
 import com.owlab.callblocker.fragment.AddFromSmsLogFragment;
 import com.owlab.callblocker.fragment.SettingsFragment;
 import com.owlab.callblocker.fragment.ViewPagerContainerFragment;
+import com.owlab.callblocker.service.CallBlockerIntentService;
 
 import java.util.Objects;
 
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String nextFragmentTag;
     private boolean onActivityResultCalled;
+    private boolean onRequestPermissionsResultCalled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,11 +87,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Because of bug in support package
+     * Without this override, the above onActivityResult will result in exceptions!
+     * http://stackoverflow.com/questions/7575921/illegalstateexception-can-not-perform-this-action-after-onsaveinstancestate-wit
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("WORKAROUND_FOR_BUG_19917_KEY", "WORKAROUND_FOR_BUG_19917_VALUE");
+        //getSupportFragmentManager().putFragment(outState, "nextFragmentTag", fragment);
+        super.onSaveInstanceState(outState);
+
+        Log.d(TAG, ">>>>> onSaveInstanceState called");
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
 
         Log.d(TAG, ">>>>> onResume");
+    }
+
+    @Override
+    public void onPostResume() {
+        super.onPostResume();
+        Log.d(TAG, "onActivityResultCalled = " + onActivityResultCalled);
+        Log.d(TAG, "onRequestPermissionsResultCalled = " + onRequestPermissionsResultCalled);
+
+        if(onActivityResultCalled) {
+            showFragmentAfterOnActivityResult(nextFragmentTag);
+            onActivityResultCalled = false;
+        }
+
+        if(onRequestPermissionsResultCalled) {
+            showFragmentAfterOnRequestPermissionsResult(nextFragmentTag);
+            onRequestPermissionsResultCalled = false;
+        }
     }
 
     @Override
@@ -151,12 +189,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        //delegate
-        FUNS.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
-    }
-
-    @Override
     public void onBackPressed() {
         super.onBackPressed();
         Log.d(TAG, ">>>>> back pressed");
@@ -168,19 +200,6 @@ public class MainActivity extends AppCompatActivity {
         Switch mainOnOffSwitch = (Switch) mainOnOffSwitchLayout.getActionView().findViewById(R.id.action_main_onoff_switch);
 
         mainOnOffSwitch.setChecked(checked);
-    }
-
-
-
-
-    @Override
-    public void onPostResume() {
-        super.onPostResume();
-        Log.d(TAG, "onActivityResultCalled = " + onActivityResultCalled);
-        if(onActivityResultCalled) {
-            showNextFragmentByTag(nextFragmentTag);
-            onActivityResultCalled = false;
-        }
     }
 
     @Override
@@ -200,24 +219,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Because of bug in support package
-     * Without this override, the above onActivityResult will result in exceptions!
-     * http://stackoverflow.com/questions/7575921/illegalstateexception-can-not-perform-this-action-after-onsaveinstancestate-wit
-     *
-     * @param outState
-     */
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putString("WORKAROUND_FOR_BUG_19917_KEY", "WORKAROUND_FOR_BUG_19917_VALUE");
-        //getSupportFragmentManager().putFragment(outState, "nextFragmentTag", fragment);
-        super.onSaveInstanceState(outState);
-
-        Log.d(TAG, ">>>>> onSaveInstanceState called");
-    }
-
-    private void showNextFragmentByTag(String tag) {
-        Log.d(TAG, ">>>>> next: " + tag);
+    private void showFragmentAfterOnActivityResult(String tag) {
+        Log.d(TAG, ">>>>> tag: " + tag);
         if(tag == null) {
             return;
         }
@@ -252,6 +255,117 @@ public class MainActivity extends AppCompatActivity {
             //addByManualDialogFragment.setTargetFragment(viewPagerContainer, 0);
             addByManualDialogFragment.setTargetFragment(currentPageFragment != null ? currentPageFragment : viewPagerContainerFragment, 0);
             addByManualDialogFragment.show(getSupportFragmentManager(), AddByManualDialogFragment.TAG);
+        }
+    }
+
+    private void showFragmentAfterOnRequestPermissionsResult(String tag) {
+        if(ViewPagerContainerFragment.TAG.equals(tag)) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new ViewPagerContainerFragment(), ViewPagerContainerFragment.TAG)
+                    .commit();
+
+        } else {
+            throw new UnsupportedOperationException("tag should be " + ViewPagerContainerFragment.TAG + ", but " + tag);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //delegate
+        //FUNS.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+        Log.d(TAG, ">>>>> called");
+
+        //Because request of multiple permissions may not result in the same number of permissions granted!
+        boolean permissionReadPhoneStateGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED;
+        boolean permissionCallPhoneGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
+        boolean permissionReadCallLogGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED;
+        boolean permissionWriteCallLogGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_CALL_LOG) == PackageManager.PERMISSION_GRANTED;
+        boolean permissionReadBlockedCallLogGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
+        //boolean permissionReadContactsGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
+        //boolean permissionWriteContactsGranted = PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) == PackageManager.PERMISSION_GRANTED;
+
+        SharedPreferences sharedPreferences = android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences(this);
+
+        switch (requestCode) {
+            case CONS.REQUEST_CODE_ASK_PERMISSION_FOR_READ_BLOCKED_CALLS:
+                if(permissionReadBlockedCallLogGranted) {
+                    //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ViewPagerContainerFragment(), ViewPagerContainerFragment.TAG).commit();
+                    nextFragmentTag = ViewPagerContainerFragment.TAG;
+                    onRequestPermissionsResultCalled = true;
+                } else {
+                    Toast.makeText(this, "Lack of permission", Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+                break;
+
+            case CONS.REQUEST_CODE_ASK_PERMISSION_FOR_BLOCKING:
+                //Log.d(TAG, ">>>>> result of asking remaining " + permissions.length + " permission(s)");
+
+                boolean canStart = true;
+
+                if(sharedPreferences.getBoolean(this.getString(R.string.settings_key_suppress_ringing), false) && !permissionReadPhoneStateGranted) {
+                    canStart = false;
+                }
+                if(sharedPreferences.getBoolean(this.getString(R.string.settings_key_dismiss_call), false) && !permissionCallPhoneGranted) {
+                    canStart = false;
+                }
+                if(sharedPreferences.getBoolean(this.getString(R.string.settings_key_delete_call_log), false) &&
+                        //!(permissionReadCallLogGranted && permissionWriteCallLogGranted && permissionReadContactsGranted && permissionWriteContactsGranted)) {
+                        !(permissionReadCallLogGranted && permissionWriteCallLogGranted)) {
+                    canStart = false;
+                }
+
+                MainActivity mainActivity = (MainActivity) this;
+                if (!canStart) {
+                    //do nothing
+                    //and change the main onOff switch
+                    mainActivity.setMainOnOffSwitch(false);
+                    Toast.makeText(mainActivity, "Can not ON because of lack of permissions", Toast.LENGTH_SHORT).show();
+                } else {
+                    //otherwise start action - blcoking on
+                    CallBlockerIntentService.startActionBlockingOn(this, new ResultReceiver(new Handler()) {
+                        @Override
+                        protected void onReceiveResult(int resultCode, Bundle reuslt) {
+                            Log.d(TAG, ">>>>> result received");
+                            Toast.makeText(getBaseContext(), "Blocking " + (resultCode == CONS.RESULT_SUCCESS ? " ON" : " OFF"), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    mainActivity.setMainOnOffSwitch(true);
+                    Toast.makeText(mainActivity, "Blocking ON", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+
+            case CONS.REQUEST_CODE_ASK_PERMISSION_FOR_SUPPRESS_RINGING:
+                if (permissionReadPhoneStateGranted) {
+                    sharedPreferences.edit().putBoolean(this.getString(R.string.settings_key_suppress_ringing), true).commit();
+                } else {
+                    sharedPreferences.edit().putBoolean(this.getString(R.string.settings_key_suppress_ringing), false).commit();
+                }
+
+                break;
+
+            case CONS.REQUEST_CODE_ASK_PERMISSION_FOR_DISMISS_CALL:
+                if (permissionCallPhoneGranted) {
+                    sharedPreferences.edit().putBoolean(this.getString(R.string.settings_key_dismiss_call), true).commit();
+                } else {
+                    sharedPreferences.edit().putBoolean(this.getString(R.string.settings_key_dismiss_call), false).commit();
+                }
+
+                break;
+
+            case CONS.REQUEST_CODE_ASK_PERMISSION_FOR_DELETE_CALL_LOG:
+                if (permissionReadCallLogGranted && permissionWriteCallLogGranted) {
+                    //    if (permissionReadCallLogGranted && permissionWriteCallLogGranted && permissionReadContactsGranted && permissionWriteContactsGranted) {
+                    sharedPreferences.edit().putBoolean(this.getString(R.string.settings_key_delete_call_log), true).commit();
+                } else {
+                    sharedPreferences.edit().putBoolean(this.getString(R.string.settings_key_delete_call_log), false).commit();
+                }
+
+                break;
+
+            default:
+                Log.e(TAG, ">>>>> unsupported request code: " + requestCode);
         }
     }
 }
