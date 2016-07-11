a622a54b3e5d61af79c5f67a2da530f8c45c179b
Nemo HJ Lee <hunjae.lee@gmail.com>
2016-07-11 23:09:56 +0900

    Try: to show spin progress on startup, but fail

--- app/src/main/java/com/owlab/callquieter/SplashActivity.java
+++ app/src/main/java/com/owlab/callquieter/SplashActivity.java
@@ -3,6 +3,7 @@ package com.owlab.callquieter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 /**
  * Created by ernest on 6/26/16.
@@ -12,6 +13,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash_layout);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.startup_progressbar);
+        progressBar.setProgress(10);
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
